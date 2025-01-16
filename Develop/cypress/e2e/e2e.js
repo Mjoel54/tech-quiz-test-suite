d00a7ae13e44d624d29ec5ed19a0ee01c687a226
Mitch <mitchjoelklein@hotmail.com>
2025-01-17 06:56:37 +1100

    component test

---
+++ Develop/cypress/e2e/e2e.js
@@ -0,0 +1,17 @@
+// GIVEN I am taking a tech quiz
+// WHEN I click the start button
+// THEN the quiz starts and I am presented with a question
+
+//each test becomes a context block
+
+// WHEN I answer a question
+// THEN I am presented with another question
+
+// WHEN all questions are answered
+// THEN the quiz is over
+
+// WHEN the quiz is over
+// THEN I can view my score
+
+// WHEN the quiz is over
+// THEN I can start a new quiz
